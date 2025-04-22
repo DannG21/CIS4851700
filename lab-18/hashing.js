45f921cdd7901830facac2e676accbdcc3eb6e8f
Daniel Aparicio <166883370+DannG21@users.noreply.github.com>
2025-04-22 18:09:30 -0400

    hashing.js

--- lab-18/hashing.js
+++ lab-18/hashing.js
@@ -4,6 +4,5 @@ function hashData(data) {
   return crypto.createHash('sha256').update(data).digest('hex');
 }
 
-// Command-line support
 const input = process.argv[2] || 'Hello, World!';
 console.log('SHA-256 Hash:', hashData(input));
